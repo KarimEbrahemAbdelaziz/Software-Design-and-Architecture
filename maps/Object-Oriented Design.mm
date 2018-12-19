c783d0346eb924bd366c7a0b1256c9daf17aa31c
Tommaso Cortonesi <tommaso.cortonesi@eng.it>
2018-12-19 06:59:58 +0100

    added mind map

---
+++ maps/Object-Oriented Design.mm
@@ -0,0 +1,65 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1545064078111" ID="ID_785597893" MODIFIED="1545120033119" TEXT="Object-Oriented Design">
+<node CREATED="1545065413211" ID="ID_1923681025" MODIFIED="1545065445733" POSITION="right" TEXT="OO Analysis and Design">
+<node CREATED="1545065587630" ID="ID_330339209" MODIFIED="1545065639560" TEXT="object keep code ORGANIZED"/>
+<node CREATED="1545065615901" ID="ID_699681842" MODIFIED="1545065656482" TEXT="objects keep code FLEXIBLE"/>
+<node CREATED="1545065670037" ID="ID_820516214" MODIFIED="1545065705573" TEXT="objects allow code to be REUSED">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1545147478677" ID="ID_1523753414" MODIFIED="1545198638948" POSITION="left" TEXT="Design in the Software Process">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">It is important to allot time to form the requirements and design, even if they are not perfectly established.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1545173922767" ID="ID_970065046" MODIFIED="1545198621292" TEXT="requirements">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol">conditions or capabilities that must be implemented in a product, based on client or user request. </font>
+    </p>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol">They are the starting point of a project&#8212;you must understand what your client wants.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1545174007825" ID="ID_823674381" MODIFIED="1545174010856" TEXT="design">
+<node CREATED="1545174133096" ID="ID_330971942" MODIFIED="1545174139977" TEXT="conceptual design">
+<node CREATED="1545198908799" ID="ID_397890278" MODIFIED="1545198981476" TEXT="conceptual mok-up">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      visual notations that provide initial thoughts for how requirements will be satisfied.
+    </p>
+    <p>
+      Mock-ups for software involving user interfaces are often presented as wireframes, which are a kind of blueprint or basic visual representation of the product.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1545198981463" ID="ID_932023809" MODIFIED="1545198985919" TEXT="components"/>
+<node CREATED="1545198989331" ID="ID_1836960705" MODIFIED="1545198993878" TEXT="connections"/>
+<node CREATED="1545198998093" ID="ID_173611230" MODIFIED="1545199003519" TEXT="responsabilities"/>
+</node>
+</node>
+<node CREATED="1545174145987" ID="ID_500714995" MODIFIED="1545174153276" TEXT="technical design"/>
+</node>
+</node>
+</node>
+</map>
