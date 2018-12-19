cdfd37499b570dd39a311b85c9c09469da5fb729
Tommaso Cortonesi <tommaso.cortonesi@eng.it>
2018-12-19 15:43:10 +0100

    mind map cmt - first module

--- maps/Object-Oriented Design.mm
+++ maps/Object-Oriented Design.mm
@@ -18,8 +18,7 @@
       <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">It is important to allot time to form the requirements and design, even if they are not perfectly established.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1545173922767" ID="ID_970065046" MODIFIED="1545198621292" TEXT="requirements">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -33,8 +32,86 @@
       <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol">They are the starting point of a project&#8212;you must understand what your client wants.</font>
     </p>
   </body>
+</html></richcontent>
+<node CREATED="1545218905340" ID="ID_231921910" MODIFIED="1545218953989" TEXT="functional requirements">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>what</b>&#160;the system or application is expected to do
+    </p>
+  </body>
 </html>
 </richcontent>
+<node CREATED="1545220149053" ID="ID_501362859" MODIFIED="1545220195974" TEXT="user stories">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      As a ______, I want to ______ so that ______.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1545218914725" ID="ID_1678168215" MODIFIED="1545218964460" TEXT="non-functional requirements">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>how </b>well the system or application does what it does
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1545219001416" ID="ID_387385097" MODIFIED="1545219003261" TEXT="performance"/>
+<node CREATED="1545219016392" ID="ID_1214704149" MODIFIED="1545219018127" TEXT="resource usage"/>
+<node CREATED="1545219026413" ID="ID_174912429" MODIFIED="1545219028065" TEXT="efficiency"/>
+<node CREATED="1545219035258" ID="ID_1735974483" MODIFIED="1545219037128" TEXT="reusability"/>
+<node CREATED="1545219043516" ID="ID_1452828914" MODIFIED="1545219044877" TEXT="flexibility"/>
+<node CREATED="1545219049502" ID="ID_1254482904" MODIFIED="1545219051395" TEXT="maintainability"/>
+</node>
+<node CREATED="1545219103826" ID="ID_457134918" MODIFIED="1545219148509" TEXT="trade-offs between quality attributes">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      these attributes need to be <b>balanced</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1545219214730" ID="ID_290287363" MODIFIED="1545219235587" TEXT="performance and maintainability"/>
+<node CREATED="1545219245007" ID="ID_1180119794" MODIFIED="1545219248681" TEXT="performance and security"/>
+<node CREATED="1545219295157" ID="ID_769325532" MODIFIED="1545219306810" TEXT="communicate and determine what is acceptable to stakeholders"/>
+<node CREATED="1545219622040" ID="ID_1070653817" MODIFIED="1545219663063" TEXT="context">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      software that stores personal information, which the public can access, may have different security requirements than software that is only used by corporate employees. In order to establish context
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1545220229194" ID="ID_1072815790" MODIFIED="1545220231260" TEXT="constraints by project">
+<node CREATED="1545220245697" ID="ID_659350165" MODIFIED="1545220251125" TEXT="cost"/>
+<node CREATED="1545220258104" ID="ID_19487270" MODIFIED="1545220261377" TEXT="time"/>
+<node CREATED="1545220262931" ID="ID_1590508132" MODIFIED="1545220264830" TEXT="manpower"/>
+</node>
+</node>
 </node>
 <node CREATED="1545174007825" ID="ID_823674381" MODIFIED="1545174010856" TEXT="design">
 <node CREATED="1545174133096" ID="ID_330971942" MODIFIED="1545174139977" TEXT="conceptual design">
@@ -51,14 +128,78 @@
       Mock-ups for software involving user interfaces are often presented as wireframes, which are a kind of blueprint or basic visual representation of the product.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1545198981463" ID="ID_932023809" MODIFIED="1545198985919" TEXT="components"/>
 <node CREATED="1545198989331" ID="ID_1836960705" MODIFIED="1545198993878" TEXT="connections"/>
 <node CREATED="1545198998093" ID="ID_173611230" MODIFIED="1545199003519" TEXT="responsabilities"/>
 </node>
+<node CREATED="1545220477105" ID="ID_1058506057" MODIFIED="1545220682490">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      CRC cards
+    </p>
+  </body>
+</html>
+</richcontent>
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      a key advantage of using CRC cards is that they allow you to physically reorganize your design
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1545220686696" ID="ID_1777978638" MODIFIED="1545220688927" TEXT="class name"/>
+<node CREATED="1545220693344" ID="ID_429210535" MODIFIED="1545220697266" TEXT="responsabilities"/>
+<node CREATED="1545220698599" ID="ID_410445317" MODIFIED="1545220711635" TEXT="collaborators"/>
 </node>
-<node CREATED="1545174145987" ID="ID_500714995" MODIFIED="1545174153276" TEXT="technical design"/>
+</node>
+<node CREATED="1545174145987" ID="ID_500714995" MODIFIED="1545218478332" TEXT="technical design">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol">breaking down components more and more into further components, each with specific responsibilities, you get down to a level where you can do a detailed design of a particular component</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1545217545937" ID="ID_44505133" MODIFIED="1545217638905" TEXT="technical diagrams">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol">visualize how to address specific issues for each component.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1545219586974" ID="ID_1372675179" MODIFIED="1545219694272" TEXT="consequences">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      an idea that seems to work fine for a small amount of data may be impractical for large amounts of data
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
 </node>
 </node>
 </node>
