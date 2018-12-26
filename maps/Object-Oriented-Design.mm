f250a20232bf72c9ac130f69050fb91ee2dce310
Tommaso Cortonesi <tommaso.cortonesi@eng.it>
2018-12-26 08:46:51 +0100

    mind map

--- maps/Object-Oriented-Design.mm
+++ maps/Object-Oriented-Design.mm
@@ -11,8 +11,7 @@
       <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol">It is important to allot time to form the requirements and design, even if they are not perfectly established.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1545173922767" ID="ID_970065046" MODIFIED="1545231751316" TEXT="requirements">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -37,8 +36,7 @@
       <b>what</b>&#160;the system or application is expected to do
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1545220149053" ID="ID_501362859" MODIFIED="1545220195974" TEXT="user stories">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -49,8 +47,7 @@
       As a ______, I want to ______ so that ______.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1545218914725" ID="ID_1678168215" MODIFIED="1545218964460" TEXT="non-functional requirements">
@@ -63,8 +60,7 @@
       <b>how </b>well the system or application does what it does
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1545219001416" ID="ID_387385097" MODIFIED="1545219003261" TEXT="performance"/>
 <node CREATED="1545219016392" ID="ID_1214704149" MODIFIED="1545219018127" TEXT="resource usage"/>
 <node CREATED="1545219026413" ID="ID_174912429" MODIFIED="1545219028065" TEXT="efficiency"/>
@@ -82,8 +78,7 @@
       these attributes need to be <b>balanced</b>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1545219214730" ID="ID_290287363" MODIFIED="1545219235587" TEXT="performance and maintainability"/>
 <node CREATED="1545219245007" ID="ID_1180119794" MODIFIED="1545219248681" TEXT="performance and security"/>
 <node CREATED="1545219295157" ID="ID_769325532" MODIFIED="1545219306810" TEXT="communicate and determine what is acceptable to stakeholders"/>
@@ -97,8 +92,7 @@
       software that stores personal information, which the public can access, may have different security requirements than software that is only used by corporate employees. In order to establish context
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1545220229194" ID="ID_1072815790" MODIFIED="1545220231260" TEXT="constraints by project">
 <node CREATED="1545220245697" ID="ID_659350165" MODIFIED="1545220251125" TEXT="cost"/>
@@ -118,8 +112,7 @@
       uses <b>object-oriented analysis</b>&#160;to identify the key objects in the problem and breaks down the problem into manageable pieces
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1545198908799" ID="ID_397890278" MODIFIED="1545198981476" TEXT="conceptual mok-up">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -148,8 +141,7 @@
       CRC cards
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -159,8 +151,7 @@
       a key advantage of using CRC cards is that they allow you to physically reorganize your design
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1545220686696" ID="ID_1777978638" MODIFIED="1545220688927" TEXT="class name"/>
 <node CREATED="1545220693344" ID="ID_429210535" MODIFIED="1545220697266" TEXT="responsabilities"/>
 <node CREATED="1545220698599" ID="ID_410445317" MODIFIED="1545220711635" TEXT="collaborators"/>
@@ -176,8 +167,7 @@
       uses <b>object-oriented design </b>to further refine the details of the objects, including their attributes and behaviours, so it is clear enough for developers to implement as working software
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1545217545937" ID="ID_44505133" MODIFIED="1545217638905" TEXT="technical diagrams">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -188,8 +178,7 @@
       <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol">visualize how to address specific issues for each component.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1545219586974" ID="ID_1372675179" MODIFIED="1545219694272" TEXT="consequences">
 <richcontent TYPE="NOTE"><html>
@@ -201,8 +190,7 @@
       an idea that seems to work fine for a small amount of data may be impractical for large amounts of data
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
@@ -225,8 +213,7 @@
       that receive events and co-ordinate actions as the process moves to the solution space
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1545231998280" ID="ID_316886841" MODIFIED="1545232291219" TEXT="boundary">
 <richcontent TYPE="NOTE"><html>
@@ -238,13 +225,12 @@
       that connect outside services to your system, as the process moves towards the solution space
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
 <node CREATED="1545232295599" ID="ID_1641438800" MODIFIED="1545232299472" POSITION="right" TEXT="design principle">
-<node CREATED="1545232304014" ID="ID_99377629" MODIFIED="1545232711719" TEXT="abstraction">
+<node CREATED="1545232304014" ID="ID_99377629" MODIFIED="1545809095412" TEXT="abstraction">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -254,12 +240,11 @@
       Abstraction breaks a concept down into a simplified description that ignores unimportant details and emphasizes the essentials needed for the concept, within some context.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1545232711696" ID="ID_1786730645" MODIFIED="1545232752062" TEXT="basic attributes"/>
 <node CREATED="1545232717841" ID="ID_1007333959" MODIFIED="1545232755508" TEXT="basic behaviour/responsabilities"/>
 </node>
-<node CREATED="1545232314391" ID="ID_1361526935" MODIFIED="1545232318506" TEXT="encapsulation">
+<node CREATED="1545232314391" ID="ID_1361526935" MODIFIED="1545809097577" TEXT="encapsulation">
 <node CREATED="1545232898859" ID="ID_374137717" MODIFIED="1545233691821" TEXT="integrity and security"/>
 <node CREATED="1545232907252" ID="ID_1975666575" MODIFIED="1545232916249" TEXT="changable implementation"/>
 <node CREATED="1545232921883" ID="ID_1161104322" MODIFIED="1545233847781" TEXT="black-box thinking">
@@ -272,11 +257,10 @@
       <b>abstraction barrier</b>: internal workings of a class are not relevant to the outside world
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
-<node CREATED="1545232326060" ID="ID_1851130348" MODIFIED="1545234083411" TEXT="decomposition">
+<node CREATED="1545232326060" ID="ID_1851130348" MODIFIED="1545809099242" TEXT="decomposition">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -286,9 +270,8 @@
       <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">look at the different responsibilities of a whole and evaluate how the whole can be separated into parts that each have a specific responsability</font>
     </p>
   </body>
-</html>
-</richcontent>
-<node CREATED="1545234501973" ID="ID_1190875213" MODIFIED="1545235808066" TEXT="association">
+</html></richcontent>
+<node CREATED="1545234501973" ID="ID_1190875213" MODIFIED="1545809653020" TEXT="association">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -300,19 +283,8 @@
   </body>
 </html>
 </richcontent>
-<node CREATED="1545235348726" ID="ID_312279988" MODIFIED="1545235443598">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <img src="../../../Desktop/asso.PNG" />
-  </body>
-</html>
-</richcontent>
 </node>
-</node>
-<node CREATED="1545234511037" ID="ID_716319527" MODIFIED="1545235806061" TEXT="aggregation">
+<node CREATED="1545234511037" ID="ID_716319527" MODIFIED="1545809661212" TEXT="aggregation">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -324,19 +296,8 @@
   </body>
 </html>
 </richcontent>
-<node CREATED="1545235547211" MODIFIED="1545235547211">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <img src="Object-Oriented Design_8865276284944883018.jpeg" />
-  </body>
-</html>
-</richcontent>
 </node>
-</node>
-<node CREATED="1545234518492" ID="ID_1186310444" MODIFIED="1545235803953" TEXT="composition">
+<node CREATED="1545234518492" ID="ID_1186310444" MODIFIED="1545809658032" TEXT="composition">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -348,28 +309,162 @@
   </body>
 </html>
 </richcontent>
-<node CREATED="1545235609273" MODIFIED="1545235609273">
-<richcontent TYPE="NODE"><html>
+</node>
+</node>
+<node CREATED="1545232333949" ID="ID_1162848014" MODIFIED="1545809847648" TEXT="generalization">
+<node CREATED="1545234247168" ID="ID_1344927806" MODIFIED="1545809067175" TEXT="inheritance">
+<node CREATED="1545235748220" ID="ID_1625982025" MODIFIED="1545235756041" TEXT="implementation inheritance"/>
+<node CREATED="1545235777451" ID="ID_1364781517" MODIFIED="1545235779461" TEXT="interface inheritance"/>
+</node>
+<node CREATED="1545234278698" ID="ID_1556178974" MODIFIED="1545234282404" TEXT="DRY rule"/>
+<node CREATED="1545809714349" ID="ID_392107462" MODIFIED="1545809763086" TEXT="Liskov substitution principle">
+<richcontent TYPE="NOTE"><html>
   <head>
     
   </head>
   <body>
-    <img src="Object-Oriented Design_4539079245389334382.jpeg" />
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px"><b>a subclass can replace a superclass, if and only if, the subclass does not change the functionality of the superclass</b></font>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">This means that if a subclass replaces a superclass, but replaces all the superclass behaviours with something totally different, then inheritance is being misused.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1545809848554" ID="ID_463482866" MODIFIED="1545809849872" TEXT="Am I using inheritance to simply share attributes or behaviour without further adding anything special in my subclasses?"/>
+</node>
+<node CREATED="1545809167880" ID="ID_308531490" MODIFIED="1545809177563" TEXT="design complexity">
+<node CREATED="1545809188289" ID="ID_1543872265" MODIFIED="1545809245426" TEXT="coupling">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">If a module is too reliant on other modules, then it is&#160;<em style="color: rgb(36, 41, 46); font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol; font-size: 16px; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">tightly coupled</em>&#160;to others. This is a bad design</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1545809251126" ID="ID_1083108148" MODIFIED="1545809309468" TEXT="degree">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">number of connections between the module and others. The degree should be small for coupling.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1545809262724" ID="ID_1295544312" MODIFIED="1545809320497" TEXT="ease">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">how obvious are the connections between the module and others. Connections should be easy</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1545809274076" ID="ID_1984417068" MODIFIED="1545809325212" TEXT="flexibility">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">how interchangeable the other modules are for this module. Other modules should be easily replaceable</font>
+    </p>
   </body>
 </html>
 </richcontent>
 </node>
 </node>
+<node CREATED="1545809198702" ID="ID_1690545617" MODIFIED="1545809360496" TEXT="cohesion">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">focuses on complexity within a module, and represents the clarity of the responsibilities of a module</font>
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
-<node CREATED="1545232333949" ID="ID_1162848014" MODIFIED="1545232338012" TEXT="generatlization">
-<node CREATED="1545234247168" ID="ID_1344927806" MODIFIED="1545234249717" TEXT="inheritance">
-<node CREATED="1545235748220" ID="ID_1625982025" MODIFIED="1545235756041" TEXT="implementation inheritance"/>
-<node CREATED="1545235777451" ID="ID_1364781517" MODIFIED="1545235779461" TEXT="interface inheritance">
-<node CREATED="1545235824509" ID="ID_1712654914" MODIFIED="1545235824509" TEXT=""/>
+</node>
+<node CREATED="1545809380479" ID="ID_680768362" MODIFIED="1545809458583" TEXT="separation of concerns">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">keeping the different concerns in your design separate</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1545809459724" ID="ID_774967721" MODIFIED="1545809478462" TEXT="information hiding">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">Modules should only have access to the information it needs to do its job</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1545809489290" ID="ID_1009739797" MODIFIED="1545809548416" TEXT="conceptual integrity">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">even if multiple people work on software system, it would seem cohesive and consistent as if only one mind was guiding the work.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1545809551642" ID="ID_22724002" MODIFIED="1545809573754" TEXT="communication"/>
+<node CREATED="1545809574788" ID="ID_1663924190" MODIFIED="1545809580704" TEXT="code reviews"/>
+<node CREATED="1545809590602" ID="ID_1068389779" MODIFIED="1545809591518" TEXT="using certain design principles and programming constructs"/>
+<node CREATED="1545809607017" ID="ID_1434126976" MODIFIED="1545809607790" TEXT="unifying concepts"/>
+<node CREATED="1545809613440" ID="ID_1449672263" MODIFIED="1545809614573" TEXT="having a small core group that accepts each commit to the code base."/>
 </node>
 </node>
-<node CREATED="1545234278698" ID="ID_1556178974" MODIFIED="1545234282404" TEXT="DRY rule"/>
-</node>
+<node CREATED="1545810045422" ID="ID_917913672" MODIFIED="1545810122487" POSITION="left" TEXT="model checking">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(36, 41, 46)" face="-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol" size="16px">systematic check of your system&#8217;s&#160;<strong style="font-weight: 600; color: rgb(36, 41, 46); font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol; font-size: 16px; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><b>state model</b></strong>&#160;in all its possible states</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1545810059630" ID="ID_1180919120" MODIFIED="1545810060574" TEXT="modelling phase"/>
+<node CREATED="1545810066351" ID="ID_747271632" MODIFIED="1545810066941" TEXT="running phase"/>
+<node CREATED="1545810072454" ID="ID_37374213" MODIFIED="1545810074569" TEXT="analysis phase"/>
 </node>
 </node>
 </map>
