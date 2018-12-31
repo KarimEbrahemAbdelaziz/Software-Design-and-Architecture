506c10fd0bb283fb48da87c58f5f250e331e732c
Tommaso Cortonesi <tommaso.cortonesi@eng.it>
2018-12-31 07:08:38 +0100

    factory object and factory method pattern

--- Design-Pattern/Module1/module1.md
+++ Design-Pattern/Module1/module1.md
@@ -28,4 +28,32 @@ An advantage of this version of a Singleton class is **lazy creation**. Lazy cre
 
 There are trade-offs to the Singleton design principle. If there are multiple computing threads running, there could be issues caused by the threads trying to access the shared single object.
 
-## Factory Method Pattern
+## Factory Objects & Factory Method Pattern
+
+The **Factory Method Pattern** is a *creational pattern*. In order to understand Factory Method patterns, we must first understand **factory objects**.
+
+### Factory Objects
+
+A factory object is an object whose role is to create "product" objects of particular types. Factory objects makes software easier to maintain and change, because object creation happens in the factories. The methods that use these Factories can then focus on other behaviour.
+
+Benefits:
+
+* is much simpler to add new types of an object to the object factory without modifying the client code
+* allow client code to operate on generalizations (*coding to an interface*)
+* cut out redundant code and made the software easier to modify, particularly if there are multiple clients that want to instantiate the same set of classes.
+
+### Factory Method Pattern
+
+Factory Method uses a separate "method" in the same class to create objects. The power of Factory Methods come in particular from how they create specialized product objects.
+
+This design pattern's intent is to define an interface for creating objects, but let the subclasses decide which class to instantiate.
+
+![uml](imgs\factoryMethodPattern.PNG)
+
+There should be an abstract Creator class, that contains methods that only operate on generalizations. The Factory Method is declared by the Creator abstractly, so that each Concrete Creator class is obliged to provide a Factory Method.  
+
+There should also be a subclass of the abstract Creator class, a Concrete Creator that is responsible for concrete instantiation. The Concrete Creator inherits methods from the abstract Creator.  
+
+There should be a factoryMethod in the concrete creator subclass. Every time a Concrete Creator subclass is added to the design, the factoryMethod() must be defined to make the right products. This is how the subclass "decides" to create objects.
+
+If object creation is separate from other behaviour, the code becomes cleaner to read, and easier to maintain or change. The client code is simplified. The code becomes more extensible, and inheritance allows for the specialization of object creation.
--- Design-Pattern/map/Design-Pattern.mm
+++ Design-Pattern/map/Design-Pattern.mm
@@ -12,8 +12,82 @@
   </body>
 </html>
 </richcontent>
-<node CREATED="1546016886575" ID="ID_1352315864" MODIFIED="1546016894796" POSITION="right" TEXT="Creational Pattern"/>
-<node CREATED="1546016901126" ID="ID_182184040" MODIFIED="1546016908392" POSITION="left" TEXT="Structural Pattern"/>
-<node CREATED="1546016910451" ID="ID_1716732300" MODIFIED="1546016921207" POSITION="left" TEXT="Behavioural Pattern"/>
+<node CREATED="1546016886575" ID="ID_1352315864" MODIFIED="1546234589976" POSITION="right" TEXT="Creational Pattern">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      deal with the creation or cloning new objects
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1546234524311" ID="ID_196467983" MODIFIED="1546234527140" TEXT="Singleton Pattern">
+<node CREATED="1546234653479" ID="ID_257583280" MODIFIED="1546234654841" TEXT="provide global access to a class that is restricted to one instance"/>
+<node CREATED="1546234667808" ID="ID_784493621" MODIFIED="1546234668913" TEXT="lazy creation"/>
+<node CREATED="1546234695063" ID="ID_878123454" MODIFIED="1546234704607" TEXT="trade-offs: multiple threads"/>
+</node>
+<node CREATED="1546235939828" ID="ID_397643898" MODIFIED="1546235947328" TEXT="Factory Object">
+<node CREATED="1546236011803" ID="ID_390677406" MODIFIED="1546236035556" TEXT="object whose role is to create &quot;product&quot; objects of particular types"/>
+<node CREATED="1546236061303" ID="ID_81317524" MODIFIED="1546236067033" TEXT="benefits">
+<node CREATED="1546236110101" ID="ID_1150629661" MODIFIED="1546236111340" TEXT="is much simpler to add new types of an object to the object factory without modifying the client code"/>
+<node CREATED="1546236124516" ID="ID_422802572" MODIFIED="1546236125442" TEXT="allow client code to operate on generalizations (coding to an interface)"/>
+<node CREATED="1546236126312" ID="ID_1982167182" MODIFIED="1546236145984" TEXT="cut out redundant code"/>
+</node>
+</node>
+<node CREATED="1546235948301" ID="ID_697935703" MODIFIED="1546236205249" TEXT="Factory Method Pattern">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      uses a separate &quot;method&quot; in the same class to create objects.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1546236298380" ID="ID_533317076" MODIFIED="1546236301885" TEXT="benefit">
+<node CREATED="1546236314314" ID="ID_991244808" MODIFIED="1546236337161" TEXT="code becomes cleaner to read, maintain or change"/>
+<node CREATED="1546236351341" ID="ID_1320841187" MODIFIED="1546236352607" TEXT="The client code is simplified"/>
+<node CREATED="1546236369398" ID="ID_147717477" MODIFIED="1546236370467" TEXT="The code becomes more extensible"/>
+<node CREATED="1546236380794" ID="ID_1701012891" MODIFIED="1546236381986" TEXT="inheritance allows for the specialization of object creation"/>
+</node>
+<node CREATED="1546236416754" ID="ID_861650712" MODIFIED="1546236419852" TEXT="pattern">
+<node CREATED="1546236231784" ID="ID_1692501491" MODIFIED="1546236245059" TEXT="Concrete Product"/>
+<node CREATED="1546236251040" ID="ID_176076049" MODIFIED="1546236254914" TEXT="Product"/>
+<node CREATED="1546236220409" ID="ID_1010899471" MODIFIED="1546236229160" TEXT="Concrete Creator"/>
+<node CREATED="1546236210137" ID="ID_1288423918" MODIFIED="1546236213402" TEXT="Creator"/>
+</node>
+</node>
+</node>
+<node CREATED="1546016901126" ID="ID_182184040" MODIFIED="1546234612029" POSITION="left" TEXT="Structural Pattern">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      describe how objects are connected to each other
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1546016910451" ID="ID_1716732300" MODIFIED="1546234619204" POSITION="left" TEXT="Behavioural Pattern">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      focus on how objects distribute work, and describe how each object does a single cohesive function
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
 </node>
 </map>
